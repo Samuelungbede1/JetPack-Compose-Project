2214e34cf4bf34e761f1b0a292cb638bacf3f3c8
Samuelungbede1 <samuel.onum1@gmail.com>
2021-12-10 17:42:01 +0100

    Performant List Implemented

---
+++ AndroidStudioProjects/MyFirstComposeApp/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.myfirstcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
+import kotlin.math.exp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp( ) {
+    var shouldShowOnboarding by remember{ mutableStateOf(true)}
+    if (shouldShowOnboarding){
+        OnboadrdingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    } else{
+        Greetings()
+    }
+}
+
+
+@Composable
+private  fun Greetings (names : List<String> = List(1000){"$it"}){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items (items = names){ name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember{ mutableStateOf(false)}
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    Surface(color = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+
+            OutlinedButton(onClick = { expanded.value = !expanded.value}) {
+                Text(if(expanded.value) "Show Less" else "Show More")
+
+            }
+        }
+
+
+    }
+
+}
+
+@Composable
+fun OnboadrdingScreen(onContinueClicked:()-> Unit){
+    //TODO: this state should be Hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true)}
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics CodeLab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked) {
+                Text("Continue")
+            }
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320,heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    MyFirstComposeAppTheme{
+        OnboadrdingScreen(onContinueClicked = {})
+    }
+}
+
+
+
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    MyFirstComposeAppTheme {
+        MyApp()
+    }
+}
